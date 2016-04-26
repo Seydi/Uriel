c2a7de23e325e84710739266cc32ab427daa101b
Seydi <syseydi@gmail.com>
2016-04-26 13:58:39 +0200

    back to last working push

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 $(document).ready(function(){
 
-    $('body').restive({
-          breakpoints: ['10000'],
-          classes: ['nb'],
-          turbo_classes: 'is_mobile=mobi,is_phone=phone,is_tablet=tablet,is_landscape=landscape'
-    });
 
 //To validate the phone number input
   function validatenumber(number) 
